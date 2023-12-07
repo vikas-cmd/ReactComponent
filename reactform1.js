32add1d067d88d7194946ff992c6fbb7380a4b1d
Vikas Maurya <54396740+vikas-cmd@users.noreply.github.com>
2023-12-07 16:52:34 +0530

    Create reactform1
    in this not reusble input components

---
+++ reactform1.js
@@ -0,0 +1,66 @@
+{/*Form with multiple inputFields*/}
+
+import React, { useState } from 'react';
+
+const MyForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log('Form submitted:', formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default MyForm;
